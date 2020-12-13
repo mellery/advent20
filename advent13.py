080510aa3adf08c75f8e8add6f061a91cecc17fe
Mike Ellery <mellery@gmail.com>
2020-12-13 11:47:19 -0500

    day13 part2

--- advent13.py
+++ advent13.py
@@ -1,3 +1,5 @@
+from math import gcd
+
 filename = "input13.txt"
 #filename = "input13_ex1.txt"
 
@@ -16,8 +18,19 @@ buses = lines[1].split(',')
 #buses = [67,7,'x',59,61] #1261476
 #buses = [1789,37,47,1889] #1202161486
 
-#1202161486
-#100000000000000
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
 
 def bus_dept(ts,bus):
     if ts%int(bus) == 0:
@@ -47,17 +60,19 @@ def part2(buses):
     offset = buses.index(slowest)
     print(slowest,offset)
     t = slowest
+    
     all_found = False
     while all_found == False:
-        print(t)
+        #print(t)
         all_found = True
         for b in range(0,len(buses)):
             if buses[b] != 'x' and bus_dept(t+b-offset,buses[b]) == False:
                 all_found = False
         if all_found == True:
             print("all_found",t-offset)
+                
             return True,t-offset
-            #break
+            
         t = t + slowest #int(buses[0])
         
 temp = lines[1].split(',')
@@ -68,9 +83,34 @@ for t in temp:
     else:
         buses.append(t)
 
-print(part2(buses))
-#print(part2([17,'x',13,19])) #3417
+x = 'x'
+print('---')
+#print(part2([17,x,13,19])) #3417
 #print(part2([67,7,59,61])) #754018
-#print(part2([67,'x',7,59,61])) #779210
-#print(part2([67,7,'x',59,61])) #1261476
+#print(part2([67,x,7,59,61])) #779210
+#print(part2([67,7,x,59,61])) #1261476
 #print(part2([1789,37,47,1889])) #1202161486
+#print(part2([13,x,x,41,x,x,x,x,x,x,x,x,x,641,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,661,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23]))
+
+import numpy as np
+from math import prod
+
+with open("input13.txt") as f:
+    lines = [x.strip() for x in f]
+
+arrival = int(lines[0])
+buses = [(i, int(e)) for i, e in enumerate(lines[1].split(",")) if e.isdigit()]
+
+times = [t for _, t in buses]
+b = [e - (arrival % e) for e in times]
+print(np.min(b) * times[np.argmin(b)])
+
+def crt(ns, bs):
+    # Chinese Remainder Theorem
+    # https://brilliant.org/wiki/chinese-remainder-theorem/
+    N = prod(ns)
+    x = sum(b * (N // n) * pow(N // n, -1, n) for b, n in zip(bs, ns))
+    return x % N
+
+offsets = [time-idx for idx, time in buses]
+print(crt(times, offsets))
