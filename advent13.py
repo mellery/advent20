fff4ba2ccf89c3471e63ab4ac9bac340af4a44ca
Mike Ellery <mellery@gmail.com>
2020-12-13 10:45:25 -0500

    add day13 wip

---
+++ advent13.py
@@ -0,0 +1,76 @@
+filename = "input13.txt"
+#filename = "input13_ex1.txt"
+
+file1 = open(filename, 'r') 
+lines = file1.readlines() 
+
+for l in lines:
+    temp = l.strip()
+    print(temp)
+
+time = int(lines[0])
+buses = lines[1].split(',')
+#buses = [17,'x',13,19] #3417
+#buses = [67,7,59,61] #754018
+#buses = [67,'x',7,59,61] #779210
+#buses = [67,7,'x',59,61] #1261476
+#buses = [1789,37,47,1889] #1202161486
+
+#1202161486
+#100000000000000
+
+def bus_dept(ts,bus):
+    if ts%int(bus) == 0:
+        return True
+    else:
+        return False
+
+t = time
+found = False
+while found == False:
+    for b in buses:
+        if b != 'x':
+            if bus_dept(t,b):
+                print(t,b,time%int(b))
+                print("wait",t-time,'minutes')
+                print(int(b)*(t-time))
+                found = True
+    t = t + 1
+
+def part2(buses):
+    valid = []
+    for b in buses:
+        if b != 'x':
+            valid.append(int(b))
+    print(buses)
+    slowest = max(valid)
+    offset = buses.index(slowest)
+    print(slowest,offset)
+    t = slowest
+    all_found = False
+    while all_found == False:
+        print(t)
+        all_found = True
+        for b in range(0,len(buses)):
+            if buses[b] != 'x' and bus_dept(t+b-offset,buses[b]) == False:
+                all_found = False
+        if all_found == True:
+            print("all_found",t-offset)
+            return True,t-offset
+            #break
+        t = t + slowest #int(buses[0])
+        
+temp = lines[1].split(',')
+buses = []
+for t in temp:
+    if t != 'x':
+        buses.append(int(t))
+    else:
+        buses.append(t)
+
+print(part2(buses))
+#print(part2([17,'x',13,19])) #3417
+#print(part2([67,7,59,61])) #754018
+#print(part2([67,'x',7,59,61])) #779210
+#print(part2([67,7,'x',59,61])) #1261476
+#print(part2([1789,37,47,1889])) #1202161486
