727b8101d3c64dd30f13879e25972567043d6bc5
Mike Ellery <mellery@gmail.com>
2020-12-16 12:08:44 -0500

    automate day16 part 2 greedy search

--- advent16.py
+++ advent16.py
@@ -40,88 +40,84 @@ for l in lines:
         ticket = temp.split(',')
         tickets.append(ticket)
 
-    #print(temp)
-
-#for f,v in fields.items():
-#    print(f,v)
-
-#print("\nmy ticket",myticket)
-#print('\nnearby')
-#for t in tickets:
-#    print(t)
-
 def validate_ticket(ticket):
     invalid = []
     for x in ticket:
         n = int(x)
-        #print(n)
-        any_field = False
+        
         invalid_count = 0
         for field, rule in fields.items():
-            #print(field)
-            #for r in rule:
             low = int(rule[0].split('-')[0])
             hi = int(rule[0].split('-')[1])
             low2 = int(rule[1].split('-')[0])
             hi2 = int(rule[1].split('-')[1])
-            #print(low,n,hi)
             
             if ((n >= low and n <= hi) or (n >= low2 and n <= hi2)) == False:
-                #print(n,'not valid for',field)
                 invalid_count = invalid_count + 1
-        #print(x,invalid_count,len(fields))
         if invalid_count == len(fields):
             invalid.append(n)
     return sum(invalid)
         
 ans = 0
-#print("#stating tickets",len(tickets))
 good_tickets = []
 for t in tickets:
-    #print(t)
     result = validate_ticket(t)
-    #print(result,t)
+
     if result == 0:
         good_tickets.append(t)
     ans = ans + result    
 print("answer",ans)
 
 good_tickets.append(myticket)
-#print("#valid tickets",len(good_tickets))
 
-#for t in good_tickets:
-#    print(t,validate_ticket(t))
+options = {}
+for field,rule in fields.items():
+    temp = []
+    for x in range(0,len(fields)):
+        temp.append(x)
+    options[field] = temp.copy()
 
-for field, rule in fields.items():
-    print(field)
-    for col in range(0,len(myticket)):
-        valid_count = 0
+#-----
+for x in range(0,len(fields)):
+    for field, rule in fields.items():
+        
+        if len(options[field]) > 1:
 
-        for t in good_tickets:
-            #print(t)
+            option_count = 0
+            tempopt = []
+            for col in options[field]:
+                valid_count = 0
+
+                for t in good_tickets:
+                    
+                    n = int(t[col])
+                    low = int(rule[0].split('-')[0])
+                    hi = int(rule[0].split('-')[1])
+                    low2 = int(rule[1].split('-')[0])
+                    hi2 = int(rule[1].split('-')[1])
+                    
+                    if ((n >= low and n <= hi) or (n >= low2 and n <= hi2)) == True:
+                        valid_count = valid_count + 1
             
-            n = int(t[col])
-            low = int(rule[0].split('-')[0])
-            hi = int(rule[0].split('-')[1])
-            low2 = int(rule[1].split('-')[0])
-            hi2 = int(rule[1].split('-')[1])
-            #print(low,n,hi)
-            
-            if ((n >= low and n <= hi) or (n >= low2 and n <= hi2)) == True:
-                #print(n,'valid for',field)
-                valid_count = valid_count + 1
-    
-        if valid_count == len(good_tickets):
-            print("col",col,"valid for",field)
+                if valid_count == len(good_tickets):
+                    option_count = option_count + 1
+                    tempopt.append(col)
 
-#manually figured out columns based on process of elimination of which columns were valid for which fields
-#1    departure location
-#3    departure station
-#8    departure platform
-#7    departure track
-#14    departure date
-#0    departure time
+            if option_count == 1:
+                options[field] = tempopt.copy()
+                for o, v in options.items():
+                    if tempopt[0] in v and o is not field:
+                        v.remove(tempopt[0])
+                break
 
-print(int(myticket[1])*int(myticket[3])*int(myticket[8])*int(myticket[7])*int(myticket[14])*int(myticket[0]))
+depts = []
+for o,v in options.items():
+    print(o,v)
+    if 'departure' in o:
+        depts.append(int(v[0]))
+print(depts)
 
-    
+ans = 1
+for x in depts:
+    ans = ans*int(myticket[x])
+print(ans)
