073fae3b0765f4a0895ffed2f50fbd7b524ec814
Mike <mellery@gmail.com>
2020-12-28 15:20:30 -0500

    Update advent21.py
    fix bug in part1 and add part2

--- advent21.py
+++ advent21.py
@@ -5,6 +5,18 @@ filename = "input21.txt"
 file1 = open(filename, 'r') 
 lines = file1.readlines() 
 
+def count_safe(safe):
+    total = 0
+
+    for f in food_label:
+        for s in safe:
+            zk_line = f.split(' ')
+            zk_line.sort()
+            if s in zk_line:
+                total = total + 1
+         
+    return total
+
 ingrediants = {}
 recipies = []
 all_allergens = []
@@ -28,38 +40,54 @@ for l in lines:
 
 for k in ingrediants:
     ingrediants[k] = all_allergens.copy()
-    print("ingrediant",k,"might contain",ingrediants[k])
+    #print("ingrediant",k,"might contain",ingrediants[k])
 
 for k in ingrediants:
     for f in food_label:
         parts = f.split()
-        if k not in parts and len(food_label[f]) == 1:
+        if k not in parts:# and len(food_label[f]) == 1:
             for i in food_label[f]:
                 temp = i.strip()
                 if temp in ingrediants[k]:
                     ingrediants[k].remove(temp)
     
-print("")
+#print("")
 safe = []
 for k in ingrediants:
-    print(k,ingrediants[k])
+    #print(k,ingrediants[k])
     if len(ingrediants[k]) == 0:
         safe.append(k)
 
-print("SAFE")
-print(safe)
+#print("SAFE")
+#print(safe)
 
-#safe.sort()
 
-total = 0
-for s in safe:
-    #print(s)
-    for f in food_label:
-        if s in f:
-            total = total + 1
 
-print(total)
+allergens = []
 
 #print all recipies after removing safe ingrediants
+print("")
+#safe = []
+for k in ingrediants:
+    if len(ingrediants[k]) > 0:
+        print(k,ingrediants[k])
+    if len(ingrediants[k]) == 1:
+        allergens.append((k,ingrediants[k][0]))
+    
 
-#2183 too high
+safe.sort()
+#print("\nSAFE",safe)
+print("\nNOT SAFE",allergens)
+
+#figured out remaining allergans manually for part 2
+#nfnfk	'dairy'
+#nbgklf	'eggs'
+#clvr 	'fish'
+#fttbhdr	'nuts'
+#qjxxpr 	'peanuts'
+#hdsm 	'sesame'
+#sjhds 	'soy'
+#xchzh 	'wheat'
+
+print(count_safe(safe))
+print('nfnfk,nbgklf,clvr,fttbhdr,qjxxpr,hdsm,sjhds,xchzh')
