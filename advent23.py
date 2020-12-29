9f37cb1f3875d3536e99f07e5f0a77dfda7f7349
Mike <mellery@gmail.com>
2020-12-29 14:01:40 -0500

    Update advent23.py
    add day23 part2

--- advent23.py
+++ advent23.py
@@ -1,85 +1,92 @@
+def print_cups(cups,current):
+    ans = "cups: "
+    for c in cups:
+        if current == c:
+            ans = ans + '('+str(c)+') '
+        else:
+            ans = ans + str(c) + ' '
+    print(ans)
 
-#input is clockwise
+def print_cups_dict(dcups,current):
+    ans = "dcups: "
+    for x in range(0,len(dcups)):
+        ans = ans + str(dcups[current]) + ' '
+        current = dcups[current]
+
+    print(ans)
 
 def cupgame(input,moves, part):
-    print(input,"moves",moves, "part",part)
-    cups = []
-    for i in input:
-        cups.append(int(i))
+    #print(input,"moves",moves, "part",part)
+    dcurrent = int(input[0])
+    
+    index = int(max(input))
+    while len(input) < 1000000:
+        index = index + 1
+        input.append(index)
+    #print(input)
 
-    if part == 2:
-        index = max(cups)
-        while len(cups) < 1000000:
-            index = index+1
-            cups.append(index)
+    dcups = {}
+    for x in range(0,len(input)-1):
+        dcups[int(input[x])] = int(input[x+1])
+    dcups[int(input[len(input)-1])] = int(input[0])
+
+    #print(dcups)
+    maxdcup = max(dcups)
 
     for m in range(0,moves):
         
-        if m == 0:
-            current = cups[0]
-        else:
-            nextcup = cups.index(current)+1
-            if nextcup > len(cups)-1:
-                nextcup = 0
-            current = cups[nextcup]
+        if m > 0:
+            dcurrent = dcups[dcurrent]
         
+        if m%100000==0:
+            print("-- move:",m+1,"--")
+        #print_cups_dict(dcups,dcurrent)
         
-        #print("-- move:",m+1,"--")
-        #print("current cup ", current)
+        dpickup = []
+        dp = dcups[dcurrent]
+        for x in range(0,3):
+            dpickup.append(dp)
+            dp = dcups[dp]
 
-        #print("cups:",cups)
+        #print("dpickup:",dpickup)
+
+        ddestination = dcurrent - 1
         
-        pickup = cups[cups.index(current)+1:cups.index(current)+4]
-        while len(pickup) < 3:
-            pickup.append(cups[0])
-            cups.pop(0)
+        if ddestination == 0:
+            ddestination = maxdcup #max(dcups)
 
-        for p in pickup:
-            if p in cups:
-                cups.remove(p)
+        while ddestination in dpickup:
+            ddestination = ddestination - 1
+            if ddestination == 0: #min(dcups):
+                ddestination = maxdcup #max(dcups)
         
-        #print("pickup:",pickup)
+        #print("ddestination:",ddestination)
+        #print("dcurrent",dcurrent)
+
+        templink = dcups[ddestination]
         
-        destination = current - 1
-        while destination not in cups:
-            destination = destination - 1
-            if destination < min(cups):
-                destination = max(cups)
-        #print("destination",destination)
+        dcups[ddestination] = dcups[dcurrent]
+        dcups[dcurrent] = dcups[dpickup[2]]
+        dcups[dpickup[2]] = templink
 
-        #cups.insert(cups.index(destination)+1,pickup)
-        cups[cups.index(destination)+1:cups.index(destination)+1] = pickup
+    dcurrent = dcups[dcurrent]
 
-    nextcup = cups.index(current)+1
-    if nextcup > len(cups)-1:
-        nextcup = 0
-    current = cups[nextcup]
+    #print('\n-- final --') 
+    #print_cups_dict(dcups,dcurrent)
 
-    print('\n-- final --') 
-    print('current',current)
-    print('cups:',cups)
-
-    oneloc = cups.index(1)
-    #print(cups[oneloc+1])
-    #print(cups[oneloc+2])
-    #print(cups[oneloc+1]*cups[oneloc+2])
-    #ans = ""
-    #for x in range(oneloc+1,len(cups)):
-    #    ans = ans + str(cups[x])
-    #for x in range(0,oneloc):
-    #    ans = ans + str(cups[x])
-
-    #print(ans)
-
+    print(dcups[1])
+    print(dcups[dcups[1]])
+    print(dcups[1] * dcups[dcups[1]])
     #1. crab picks up 3 times clockwise of the current cup
     #2. crab selects destination cup with label equal to current cup -1, or -1 till cup found, or wraps around
     #3. crab inserts the cups clockwise of destination cup
     #4. get new current cup
 
 
-cupgame(list('389125467'), 10, 1)
-cupgame(list('389125467'), 100, 1)
-cupgame(list('784235916'), 100, 1)
+#cupgame(list('389125467'), 10, 1)
+#cupgame(list('389125467'), 100, 1)
+#cupgame(list('784235916'), 100, 1)
 
 #cupgame(list('389125467'), 10000000, 2)
-#cupgame(list('784235916'), 10000000, 2)
+cupgame(list('784235916'), 10000000, 2)
+#cupgame(list('54321'),20,2)
